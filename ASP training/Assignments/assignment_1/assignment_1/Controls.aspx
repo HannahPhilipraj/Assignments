796f286ffa413dcd6e3dcb875088cd811fdcaa15
Hannah Philipraj <hannahp@infinite.com>
2023-10-31 20:21:19 +0530

    assignment1

---
+++ ASP training/Assignments/assignment_1/assignment_1/Controls.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Controls.aspx.cs" Inherits="assignment_1.Controls" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form" runat="server">
+        <div>
+            Select an item :
+            <asp:DropDownList ID="DropDownList" runat="server" Width="119px" OnSelectedIndexChanged="DropDownList_SelectedIndexChanged" AutoPostBack="True">
+                <asp:ListItem Text="Select" Value=" " />
+                <asp:ListItem Text="Dairy Milk" Value="dairy milk.jpg" />
+                <asp:ListItem Text="Kit kat" Value="kit kat.jpg" />
+                <asp:ListItem Text="Ferrero rocher" Value="ferrero rocher.jpg" />
+                <asp:ListItem Text="Toblerone" Value="toblerone.jpg" />
+                <asp:ListItem Text="Snickers" Value="snickers.jpg" />
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Image ID="Images" runat="server" Height="194px" Width="275px" />
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="ButtonCost" runat="server" Text="Cost" Width="60px" OnClick="Button_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label" runat="server" Text=" "></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP training/Assignments/assignment_1/assignment_1/Controls.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace assignment_1
+{
+    public partial class Controls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Images.ImageUrl = $"~/Images/default.jpg";
+            }
+        }
+
+        protected void DropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = DropDownList.SelectedValue;
+            Images.ImageUrl = $"~/Images/{selected}";            
+        }
+ 
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            string selected = DropDownList.SelectedValue;
+            switch(selected)
+            {
+                case "dairy milk.jpg":
+                    Label.Text = "$180";
+                    break;
+                case "kit kat.jpg":
+                    Label.Text = "$150";
+                    break;
+                case "ferrero rocher.jpg":
+                    Label.Text = "$300";
+                    break;
+                case "toblerone.jpg":
+                    Label.Text = "$160";
+                    break;
+                case "snickers.jpg":
+                    Label.Text = "$200";
+                    break;
+                default:
+                    Label.Text = "$0";
+                    break;
+            }
+        }
+    }
+}
