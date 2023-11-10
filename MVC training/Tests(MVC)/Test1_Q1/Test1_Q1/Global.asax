342bbeabdcdf936ef65ea25f97bf29ee107f96a5
Hannah Philipraj <hannahp@infinite.com>
2023-11-10 17:44:48 +0530

    MVC

---
+++ MVC training/Tests(MVC)/Test1_Q1/Test1_Q1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test1_Q1.MvcApplication" Language="C#" %>
---
+++ MVC training/Tests(MVC)/Test1_Q2/Test1_Q2/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test1_Q2.Models;
+
+namespace Test1_Q2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        MoviesDBEntities1 mv = new MoviesDBEntities1();
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Movie name)
+        {
+            mv.Movie.Add(name);
+            mv.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult ByDate()
+        {
+            var date = 01 / 01 / 1990;
+            List<Movie> movies = from m in mv.Movie
+                                 where m.DateOfRelease = date
+                                 select mv.Movie;
+            return View();                 
+        }
+    }
+}
